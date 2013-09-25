6b45cae6a52612c1d2f76551454890c9edd5f010
Cameron Pickham <crpickham@gmail.com>
2013-09-24 23:07:37 -0700

    fixed spacing, sorting

--- frequency.py
+++ frequency.py
@@ -3,25 +3,25 @@ import sys
 import json
 
 def main():
-	tweets = open(sys.argv[1])
-	term_dict = {}
-	term_total = 0
+  tweets = open(sys.argv[1])
+  term_dict = {}
+  term_total = 0
 
 	# Load terms into dictionary mapping (term, count)
-	for line in tweets:
-		tweet = json.loads(line)
-		if 'text' in tweet:
-			terms = tweet['text'].lower().encode('utf-8').split()
-			for term in terms:
-				if term_dict.has_key(term):
-					term_dict[term] = term_dict[term] + 1
-				else:
-					term_dict[term] = 1
-				term_total += 1
+  for line in tweets:
+    tweet = json.loads(line)
+    if 'text' in tweet:
+      terms = tweet['text'].lower().encode('utf-8').split()
+      for term in terms:
+        if term_dict.has_key(term):
+          term_dict[term] = term_dict[term] + 1
+        else:
+          term_dict[term] = 1
+        term_total += 1
 
 	# Print dictionary
-	for x in term_dict:
-		print x, ' ', term_dict[x]/term_total
+  for x in sorted(term_dict, key=term_dict.get):
+    print x, ' ', term_dict[x]/term_total
 
 if __name__ == '__main__':
-    main()
+  main()
